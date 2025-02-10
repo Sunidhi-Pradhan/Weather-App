7e1ec9df1145f1922864173244c4c2911040d213
Sunidhi-Pradhan <sunidhipradhan16@gmail.com>
2025-02-10 16:20:23 +0530

    final code

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
 const apiKey = "3beca38e0bf5bc9ff5e5642c00cc55b6";
-const city = "bangalore";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${apiKey}&q=`;
 
-async function checkWeather() {
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function checkWeather(city) {
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl + city);
         if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error('City not found');
         }
         const data = await response.json();
+
         console.log(data);
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "images/snow.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "images/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+
     } catch (error) {
-        console.error("Error fetching weather data:", error);
-    }
-
-
+         console.error(error);
+        // document.querySelector(".weather").style.display = "none";
+        alert('Invalid city entered. Please try again.');   }
 
 }
 
-checkWeather();
+searchBtn.addEventListener("click" , ()=>{
+    checkWeather(searchBox.value);
+});
+
